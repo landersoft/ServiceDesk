2337bb67593d2b5db84a25814c10d6f162afbbf9
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2019-08-08 16:50:52 -0400

    se agrega cambio de titulo

--- Planilla/urls.py
+++ Planilla/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.sites.AdminSite.site_title = 'Sistema Tickets Nivel 2'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
